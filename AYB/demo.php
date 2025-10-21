48020a80ff58dde1080f1da1d07907e56aa51d3f
Matias <rojascofrem@gmail.com>
2025-10-21 16:54:34 -0300

    add new funtionalities

--- AYB/demo.php
+++
@@ -1,6 +0,0 @@
-<?php
-
-use SebastianBergmann\Timer\Timer;
-
-require __DIR__ . '/vendor/autoload.php';
-
--- AYB/emailjs-config.js
+++ AYB/emailjs-config.js
@@ -5,29 +5,20 @@ const EMAILJS_CONFIG = {
 };
 
 // Template sugerido para EmailJS:
-/*
-Asunto: Nueva consulta de {{from_name}} - {{company}}
-
-Cuerpo del email:
--------------------
-Nueva consulta recibida desde el sitio web:
 
 Nombre: {{from_name}}
 Empresa: {{company}}
 Email: {{from_email}}
 Teléfono: {{phone}}
-Servicio de interés: {{service}}
+interés: {{service}}
 
 Mensaje:
 {{message}}
 
--------------------
-Este email fue enviado automáticamente desde el formulario de contacto del sitio web.
-*/
 
-// Función para actualizar la configuración
+let menssage = "Este email fue enviado automáticamente desde el formulario de contacto del sitio web";
+
 function updateEmailJSConfig() {
-    // Inicializar EmailJS con la configuración
     if (typeof emailjs !== 'undefined') {
         emailjs.init(EMAILJS_CONFIG.PUBLIC_KEY);
     }
